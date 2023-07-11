7ff562425c9e85e8220229db83c04f9d1a0ada11
invdev <inv.dev778@gmail.com>
2023-07-11 16:40:59 +0900

    cors 수정(모두 허용)

--- src/main/java/com/project/StudyWithUs/common/config/WebConfig.java
+++ src/main/java/com/project/StudyWithUs/common/config/WebConfig.java
@@ -1,16 +1,20 @@
 package com.project.StudyWithUs.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // React 앱이 실행되는 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedOrigins("*") // React 앱이 실행되는 도메인
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3000);
     }
 }
